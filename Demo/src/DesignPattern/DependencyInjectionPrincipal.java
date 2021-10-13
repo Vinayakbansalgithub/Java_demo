5e597943836d7ef349ce3a5cbfeec9c2eb53c36d
Vinayak Bansal <vinayak@functionize.com>
2021-10-13 16:50:19 +0530

    new update

--- Demo/src/DesignPattern/DependencyInjectionPrincipal.java
+++ Demo/src/DesignPattern/DependencyInjectionPrincipal.java
@@ -4,3 +4,12 @@ public class DependencyInjectionPrincipal {
 
 }
 //https://medium.com/@kedren.villena/simplifying-dependency-inversion-principle-dip-59228122649a
+
+//
+//High-level modules should not depend on low-level modules. Both should depend on abstractions.
+//Abstractions should not depend on details. Details should depend on abstractions.
+//
+//public interface ICalculatorOperation
+//{
+//    double Calculate(double x, double y);
+//}
